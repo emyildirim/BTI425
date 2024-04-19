c8a1d44584fe7a6a6babd24c63bb7fdd284bad9b
Erkam <105301175+emyildirim@users.noreply.github.com>
2024-04-18 22:39:07 -0400

    Add files via upload

---
+++ A2/my-app/components/Counter.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function ClickCounter(props) {
+    const [numClicks, setNumClicks] = useState(0);
+
+    function increaseNumClicks(e) { // 'e' is the current event object
+        setNumClicks(numClicks + 1);
+    }
+
+    return <button onClick={increaseNumClicks}>Clicks: {numClicks}</button>
+}
